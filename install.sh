233534008fe970cb48112831a49df9b21f05a922
liberodark <liberodark@gmail.com>
2019-06-03 15:50:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,23 +36,43 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 app=linux-clock
 
 #==============================================
-# INSTALL NET-TOOLS
-#==============================================
-
-#apt install -y date
-
-#==============================================
 # INSTALL SERVICE
 #==============================================
-echo Install $app service
 
+which wget &> /dev/null
+if [ "$?" != 0 ]; then
+echo "wget is not Installed"
+echo Install $app service
 echo "
 [Unit]
 Description=$app
 After=network.target
 
 [Service]
-WorkingDirectory=$final_path
+WorkingDirectory=/usr/bin/
+User=root
+Group=users
+Type=simple
+UMask=000
+ExecStart=/usr/bin/date +"%d %b %Y %T %Z" -s "$(curl -s --head https://google.com | grep '^Date:' | cut -d' ' -f 3-)"
+TimeoutSec=30
+RestartSec=15s
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/$app.service
+
+else
+
+echo "wget is Installed"
+echo Install $app service
+echo "
+[Unit]
+Description=$app
+After=network.target
+
+[Service]
+WorkingDirectory=/usr/bin/
 User=root
 Group=users
 Type=simple
@@ -65,10 +85,12 @@ Restart=always
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/$app.service
 
+fi
+
 #=================================================
-# SETUP APP
+# SETUP SERVICE
 #=================================================
 
-echo Enable services
+echo "Enable services"
 systemctl enable $app.service
 systemctl start $app.service
